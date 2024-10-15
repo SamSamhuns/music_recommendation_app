f646a6ecd05d6cb5c613daf22bc4628cb830a2c2
SamSamhuns <samhunsadamant@gmail.com>
2024-10-15 15:20:50 +0400

    Add setup & cfg

---
+++ app/core/config.py
@@ -0,0 +1,48 @@
+"""
+Load configurations and constants 
+"""
+import os
+from logging.config import dictConfig
+from models.logging import LogConfig
+from dotenv import load_dotenv
+
+# load environment variables from .env file
+load_dotenv(override=True)
+
+# project information
+PROJECT_NAME: str = "Music Analysis API template"
+PROJECT_DESCRIPTION: str = "Template API for Music Analysis"
+DEBUG: bool = os.environ.get("DEBUG", "") != "False"
+VERSION: str = "0.0.1"
+
+# server settings
+API_SERVER_PORT = int(os.getenv("API_SERVER_PORT", default="8080"))
+
+# save directories
+ROOT_STORAGE_DIR = os.getenv("ROOT_STORAGE_DIR", default="volumes/music_analysis")
+FILE_STORAGE_DIR = os.getenv("FILE_STORAGE_DIR", default=os.path.join(ROOT_STORAGE_DIR, "files"))
+VECTOR_STORE_DIR = os.getenv("VECTOR_STORE_DIR", default=os.path.join(ROOT_STORAGE_DIR, "vector_store"))
+LOG_STORAGE_DIR = os.getenv("LOG_STORAGE_DIR", default=os.path.join(ROOT_STORAGE_DIR, "logs"))
+
+os.makedirs(ROOT_STORAGE_DIR, exist_ok=True)
+os.makedirs(FILE_STORAGE_DIR, exist_ok=True)
+os.makedirs(LOG_STORAGE_DIR, exist_ok=True)
+
+# logging conf
+log_cfg = LogConfig()
+# override info & error log paths
+log_cfg.handlers["info_rotating_file_handler"]["filename"] = os.path.join(LOG_STORAGE_DIR, "info.log")
+log_cfg.handlers["warning_file_handler"]["filename"] = os.path.join(LOG_STORAGE_DIR, "error.log")
+log_cfg.handlers["error_file_handler"]["filename"] = os.path.join(LOG_STORAGE_DIR, "error.log")
+dictConfig(log_cfg.model_dump())
+
+# mysql conf
+POSTGRES_HOST = os.getenv("POSTRES_HOST", default="0.0.0.0")
+POSTGRES_PORT = int(os.getenv("POSTGRES_PORT", default="5432"))
+POSTGRES_USER = os.getenv("POSTGRES_USER", default="user")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", default="pass")
+POSTGRES_DATABASE = os.getenv("POSTGRES_DATABASE", default="default")
+
+# Spotify API conf
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
---
+++ app/core/setup.py
@@ -0,0 +1,59 @@
+"""
+Setup connections
+"""
+import base64
+from typing import Callable
+
+import requests
+import psycopg
+from core.config import (
+    POSTGRES_HOST, POSTGRES_PORT, POSTGRES_USER,
+    POSTGRES_PASSWORD, POSTGRES_DATABASE,
+    CLIENT_ID, CLIENT_SECRET)
+from contextlib import contextmanager
+
+
+######## set up postgres connection #########
+
+def get_postgres_connection() -> psycopg.Connection:
+    """Return psycopg3 connection object for PostgreSQL."""
+    return psycopg.connect(
+        host=POSTGRES_HOST,
+        port=POSTGRES_PORT,
+        user=POSTGRES_USER,
+        password=POSTGRES_PASSWORD,
+        dbname=POSTGRES_DATABASE
+    )
+
+
+@contextmanager
+def postgres_conn() -> Callable:
+    """Yield psycopg3 connection object."""
+    conn = get_postgres_connection()
+    try:
+        yield conn
+    finally:
+        conn.close()
+
+
+######## set up spotify api access token #########
+
+# Use Base64 to encode the client ID and client secret
+credentials = f"{CLIENT_ID}:{CLIENT_SECRET}"
+credentials_base64 = base64.b64encode(credentials.encode())
+
+# Make a request for the access token
+TOKEN_URL = 'https://accounts.spotify.com/api/token'
+headers = {
+    'Authorization': f'Basic {credentials_base64.decode()}'
+}
+data = {
+    'grant_type': 'client_credentials'
+}
+response = requests.post(TOKEN_URL, data=data, headers=headers, timeout=100)
+
+if response.status_code == 200:
+    spot_access_token = response.json()['access_token']
+    print("Spotify API Access token successfully obtained.")
+else:
+    print("Spotify API Access token not obtained.")
